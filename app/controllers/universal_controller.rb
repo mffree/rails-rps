e84f2dd393b1f6c1a3101d0e27aa39ca25603e99
mffree <matthewfreeman63@gmail.com>
2025-02-06 06:28:28 +0000

    final version

--- app/controllers/universal_controller.rb
+++ app/controllers/universal_controller.rb
@@ -15,6 +15,38 @@ class UniversalController < ApplicationController # precreated class that we wan
     else
       @outcome = "won"
     end
+    render({ :template => "all_views_MF/results_view_MF" }) # these render lines are all the same because I originally designed the HTML pages to accept the same input (using instance variables) regardless of which move the page was for
+  end
+
+  def paper
+    moves = ["rock", "paper", "scissors"]
+    @my_move = "paper"
+    @comp_move = moves.sample
+    
+    if @comp_move == "rock"
+      @outcome = "won"
+    elsif @comp_move == "paper"
+      @outcome = "tied"
+    else
+      @outcome = "lost"
+    end
     render({ :template => "all_views_MF/results_view_MF" })
   end
+
+  def scissors
+    moves = ["rock", "paper", "scissors"]
+    @my_move = "scissors"
+    @comp_move = moves.sample
+    
+    if @comp_move == "rock"
+      @outcome = "lost"
+    elsif @comp_move == "paper"
+      @outcome = "won"
+    else
+      @outcome = "tied"
+    end
+
+    render({ :template => "all_views_MF/results_view_MF" })
+  end
+
 end
